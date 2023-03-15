7f304bed554db022429087cee6b4d3deadb1af28
Abhishek Guha <qohor2525@gmail.com>
2023-03-16 00:14:03 +0530

    joi password min length fix

--- services/validation.js
+++ services/validation.js
@@ -8,7 +8,7 @@ const userDataFormSchema = Joi.object({
         'string.min': 'username should be greater than 2 char: min 3 allowed',
         'string.max': 'username should be less than 11 char: max 10 allowed',
     }),
-    password: Joi.string().trim().min(4).max(10).required().messages({
+    password: Joi.string().trim().min(3).max(10).required().messages({
         'any.required': 'password key/val missing',
         'string.base': 'password should be string',
         'string.empty': 'password is not allowed to be empty',
